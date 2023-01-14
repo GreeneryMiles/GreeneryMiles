142ae9d15d96368008aef999aec19ad205b1def2
tanusrixh <118092429+tanusrixh@users.noreply.github.com>
2023-01-14 14:43:52 -0500

    Update App.js

--- greenerymiles_client/client/src/App.js
+++ greenerymiles_client/client/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 const mapStyles = {
-  width: '80%',
-  height: '80%'
+  width: '1000px',
+  height: '1000px',
+  padding: '50px'
 };
-export class App extends Component {
+class App extends Component {
   render() {
     return (
       <Map
