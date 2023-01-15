c0924c577dd65f592eec97adbb8864486b361cdc
tanusrixh <118092429+tanusrixh@users.noreply.github.com>
2023-01-14 20:19:50 -0500

    Update App.js

--- greenerymiles_client/client/src/App.js
+++ greenerymiles_client/client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
-import pin from './pngegg.png';
 import jsondata from './housedata.json';
 
 const mapStyleRef = {
@@ -13,26 +12,45 @@ const markerStyleRef = {
   height: '50px'
 };
 
-class App extends Component {
+export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stores: [{lat: 43.004198, lng: -81.2652997},
+              {latitude: 42.9977861, longitude: -81.2810638},
+              {latitude: 43.0038463, longitude: -81.2674126},
+              {latitude: 43.00146, longitude: -81.28345999999999},
+              {latitude: 43.0020065, longitude: -81.28597049999999},
+              {latitude: 43.0058789, longitude: -81.267133}]
+    }
+  }
+
+  displayMarkers = () => {
+    return this.state.stores.map((store, index) => {
+      return <Marker key={index} id={index} position={{
+       lat: store.latitude,
+       lng: store.longitude
+     }}
+     onClick={() => console.log("You clicked me!")} />
+    })
+  }
+
   render() {
     return (
-      <Map
-        google={this.props.google}
-        zoom={10}
-        style={mapStyleRef}
-        initialCenter={{lat: 43.009320, lng: -81.271730}}
-
-      />
+        <Map
+          google={this.props.google}
+          zoom={15}
+          style={mapStyleRef}
+          initialCenter={{ lat: 43.009319, lng: -81.271729}}
+        >
+          {this.displayMarkers()}
+        </Map>
     );
   }
 }
 
-function marker(){
-
-}
 
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyDsB764Ax1QjNFQ2_rSbwDaPJLlU81JBYc'
 })(App);
-
-
