31ef06727d738e88a9db0370b7fc7bf614637911
tanusrixh <118092429+tanusrixh@users.noreply.github.com>
2023-01-14 21:16:37 -0500

    Update App.js

--- greenerymiles_client/client/src/App.js
+++ greenerymiles_client/client/src/App.js
@@ -8,10 +8,6 @@ const mapStyleRef = {
   position: 'absolute'
 };
 
-const markerStyleRef = {
-  width: '50px',
-  height: '50px'
-};
 
 export class App extends Component {
   constructor(props) {
@@ -20,7 +16,13 @@ export class App extends Component {
     this.state = {
       showingInfoWindow: false,  // Hides or shows the InfoWindow
       activeMarker: {},          // Shows the active marker upon click
-      selectedPlace: {}          // Shows the InfoWindow to the selected place upon a marker
+      selectedPlace: {},          // Shows the InfoWindow to the selected place upon a marker
+      stores: [{latitude: jsondata.data[1].lat, longitude: jsondata.data[1].lng, addresses:jsondata.data[1].address},
+        {latitude: jsondata.data[2].lat, longitude: jsondata.data[2].lng, addresses:jsondata.data[2].address},
+        {latitude: jsondata.data[3].lat, longitude: jsondata.data[3].lng, addresses:jsondata.data[3].address},
+        {latitude: jsondata.data[4].lat, longitude: jsondata.data[4].lng, addresses:jsondata.data[4].address},
+        {latitude: jsondata.data[5].lat, longitude: jsondata.data[5].lng, addresses:jsondata.data[5].address},
+        {latitude: jsondata.data[6].lat, longitude: jsondata.data[6].lng, addresses:jsondata.data[6].address}]
     };
   }
 
@@ -40,6 +42,16 @@ onClose = props => {
   }
 };
 
+displayMarkers = () => {
+  return this.state.stores.map((store, index) => {
+    return <Marker key={index} id={index} position={{
+     lat: store.latitude,
+     lng: store.longitude
+   }} name={store.addresses}
+   onClick={this.onMarkerClick} />
+  })
+}
+
   render() {
     return (
         <Map
@@ -47,12 +59,15 @@ onClose = props => {
           zoom={15}
           style={mapStyleRef}
           initialCenter={{ lat: jsondata.data[0].lat, lng: jsondata.data[0].lng}}
+
         >
+          {this.displayMarkers()}
           <Marker
           onClick={this.onMarkerClick}
           name={jsondata.data[0].address}
           
         />
+         
         <InfoWindow
           marker={this.state.activeMarker}
           visible={this.state.showingInfoWindow}
